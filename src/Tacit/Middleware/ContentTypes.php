ac0ac8962a82ad295faf11f7df66db4999e9fc4b
Jan Peca <pecajan@gmail.com>
2016-03-02 13:20:41 +0100

    Fix ContentType middleware to return array for empty body

--- src/Tacit/Middleware/ContentTypes.php
+++ src/Tacit/Middleware/ContentTypes.php
@@ -24,4 +24,45 @@ class ContentTypes extends \Slim\Middleware\ContentTypes
         }
         $this->next->call();
     }
+
+    /**
+     * Parse input
+     *
+     * This method will attempt to parse the request body
+     * based on its content type if available.
+     *
+     * @param  string $input
+     * @param  string $contentType
+     * @return mixed
+     */
+    protected function parse ($input, $contentType)
+    {
+        if (isset($this->contentTypes[$contentType]) && is_callable($this->contentTypes[$contentType])) {
+            $result = call_user_func($this->contentTypes[$contentType], $input);
+            if ($result !== false) {
+                return $result;
+            }
+        }
+
+        return $input;
+    }
+
+    /**
+     * Parse JSON
+     *
+     * This method converts the raw JSON input
+     * into an associative array.
+     *
+     * @param  string       $input
+     * @return array|string
+     */
+    protected function parseJson($input)
+    {
+        if (function_exists('json_decode')) {
+            $result = json_decode($input, true);
+            if(json_last_error() === JSON_ERROR_NONE) {
+                return !empty($result) ? $result : [];
+            }
+        }
+    }
 }
